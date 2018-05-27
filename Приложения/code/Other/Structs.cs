d8c0e1a60b1e00d6cee8ba19b75dac467aeaf409
funbotan <funbotan@gmail.com>
2018-05-27 22:38:22 +0300

    upd

---
+++ Приложения/code/Other/Structs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSI_GIS
+{
+    struct Jump
+    {
+        public int fromPoly;
+        public int toPoly;
+        public int returnFrom;
+        public int returnTo;
+    }
+
+    struct Connection
+    {
+        public int v0;
+        public int v1;
+        public double len;
+    }
+}
---
+++ Приложения/code/Polygons/InnerPoly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSI_GIS
+{
+    // Раньше он был нужен
+    class InnerPoly : Poly
+    {
+        public InnerPoly(string arg) : base(arg) { }
+    }
+}
